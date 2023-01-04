d5af41807c0417be5d15cea65341984c2d60c550
picocodes <picocodes@gmail.com>
2023-01-04 09:43:19 +0300

    wrong PHP snippet shows up

--- includes/forms/views/metabox-embed.php
+++ includes/forms/views/metabox-embed.php
@@ -11,7 +11,7 @@
 <div class="noptin-text-wrapper">
 	<label>
 		<strong><?php esc_html_e( 'PHP Snippet', 'newsletter-optin-box' ); ?></strong>
-		<input type="text" class="code" value="<?php echo esc_attr( sprintf( '<?php echo show_noptin_form(%d); ?>', (int) $GLOBALS['post_ID'] ) ); ?>]" readonly="readonly" onclick="this.select()" style="width: 100%;">
+		<input type="text" class="code" value="<?php echo esc_attr( sprintf( '<?php show_noptin_form(%d); ?>', (int) $GLOBALS['post_ID'] ) ); ?>" readonly="readonly" onclick="this.select()" style="width: 100%;">
         <p class="description"><?php esc_html_e( 'Use this PHP snippet to display the form inside your theme template files.', 'newsletter-optin-box' ); ?></p>
 	</label>
 </div>
