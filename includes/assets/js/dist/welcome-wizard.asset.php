5a9d38790d325ba639749503851c0b7d1a14ee0f
Lewis ushindi <coderflame3@gmail.com>
2023-05-11 10:27:50 +0300

    update wizard structure

--- includes/assets/js/dist/welcome-wizard.asset.php
+++ includes/assets/js/dist/welcome-wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f287fb8130a074b4d3c5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '196aabffc9180278a127');
--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,11 @@ module.exports = {
 						cacheDirectory: true
 					}
 				}
-			}
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			  },
 		]
 	},
 	plugins: [
