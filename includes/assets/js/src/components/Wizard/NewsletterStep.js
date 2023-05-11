7d07327be78330f27b16ab3f382a26fee1e6299d
Lewis ushindi <coderflame3@gmail.com>
2023-05-11 10:19:38 +0300

    updating wizard code structure

--- includes/assets/js/src/components/Wizard/NewsletterStep.js
+++ includes/assets/js/src/components/Wizard/NewsletterStep.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 
 function NewsletterStep(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCreateNewsletter = () => {
+    // Code for creating and sending the newsletter
+    // Replace this with your actual implementation
+
+    // Show the modal
+    setShowModal(true);
+
+    // Close the modal after 2 seconds
+    setTimeout(() => {
+      setShowModal(false);
+    }, 2000);
+  };
+
   return (
     <div className="noptin-welcome-wizard-newsletter">
       <h2>{__('Step 2: Create Your First Newsletter', 'noptin')}</h2>
       <p>{__('Use the form below to create and send your first email newsletter to your subscribers.', 'noptin')}</p>
-      {/* Add code for creating and sending newsletters here */}
+      <button className="noptin-welcome-wizard-button" onClick={props.onPreviousStep}>
+        {__('Back', 'noptin')}
+      </button>
+      <button className="noptin-welcome-wizard-button noptin-welcome-wizard-button-primary" onClick={handleCreateNewsletter}>
+        {__('Create and Send Newsletter', 'noptin')}
+      </button>
       <div className="noptin-welcome-wizard-buttons">
         <button className="noptin-welcome-wizard-button" onClick={props.onPreviousStep}>
           {__('Back', 'noptin')}
@@ -14,6 +35,13 @@ function NewsletterStep(props) {
           {__('Next', 'noptin')}
         </button>
       </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>{__('Newsletter created and sent!', 'noptin')}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
