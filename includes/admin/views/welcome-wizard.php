807ebd6c8ee0cc3ca7665e2390d662298523fcf8
picocodes <picocodes@gmail.com>
2023-05-14 18:40:27 +0300

    add wizzard admin page

---
+++ includes/admin/views/welcome-wizard.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Displays a welcome wizzard.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<div class="wrap noptin-welcome-wizzard-page" id="noptin-wrapper">
+	<div id="noptin-welcome-wizard" class="noptin-es6-app">
+		<?php esc_html_e( 'Loading...', 'newsletter-optin-box' ); ?>
+		<span class="spinner"></span>
+	</div>
+</div>
