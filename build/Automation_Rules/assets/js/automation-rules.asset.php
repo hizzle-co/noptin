9f632037a32117f51485a0eb0c5aced49d3627df
picocodes <picocodes@gmail.com>
2024-02-25 08:30:12 +0300

    fixes #762

--- build/Automation_Rules/assets/js/automation-rules.asset.php
+++ build/Automation_Rules/assets/js/automation-rules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '78ed4e2152d3954e3ab6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8b710aa5af28fe47b267');
--- build/Emails/assets/js/global.asset.php
+++ build/Emails/assets/js/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'b367180bdde580827bf7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'e8f35a2607bdfd47252f');
