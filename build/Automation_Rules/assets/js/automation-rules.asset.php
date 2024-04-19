3d123f802a22e95cbe69ed7cc46dbaa0f49f6c66
picocodes <picocodes@gmail.com>
2024-04-19 12:18:31 +0300

    build

--- build/Automation_Rules/assets/js/automation-rules.asset.php
+++ build/Automation_Rules/assets/js/automation-rules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fed74a910a3d094e4b22');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9b3084a6e3c2ca092e87');
--- build/Emails/assets/js/view-campaigns.asset.php
+++ build/Emails/assets/js/view-campaigns.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6c4d78a8e2403fe7c5a1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b933a5f7a226f716a24a');
