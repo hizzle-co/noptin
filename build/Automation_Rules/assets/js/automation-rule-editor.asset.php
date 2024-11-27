f0e8f2d4a6e0a314be3a4e4dc6a274f2c095ac23
picocodes <picocodes@gmail.com>
2024-11-27 10:45:05 +0300

    changes

--- build/Automation_Rules/assets/js/automation-rule-editor.asset.php
+++ build/Automation_Rules/assets/js/automation-rule-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => '4f8db967c99b2060d0ae');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => 'cb6096e9693d53184b8a');
--- build/Emails/assets/js/global.asset.php
+++ build/Emails/assets/js/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'b32d6a32ef9206d020a3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '4cd9e1bfe5e2ffb61814');
