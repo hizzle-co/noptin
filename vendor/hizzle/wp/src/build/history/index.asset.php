d683764e99f16f5332cbe324ffebc58499c6ced2
picocodes <picocodes@gmail.com>
2025-11-17 15:21:23 +0300

    composer update

--- vendor/hizzle/wp/src/build/history/index.asset.php
+++ vendor/hizzle/wp/src/build/history/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-url'), 'version' => 'c20c33a7f712296536ae');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-url'), 'version' => '423dee02b385f4922c85');
