ce5a550d3f2c61c4aac8af95b0283ad41d12e464
picocodes <picocodes@gmail.com>
2026-01-21 21:03:44 +0300

    Add additional parameters to newsletter recipients filter

--- src/Objects/People_List.php
+++ src/Objects/People_List.php
@@ -117,7 +117,7 @@ class People_List extends \Hizzle\Noptin\Emails\Bulk\Sender {
 		// Adjust batch size if there's a limit.
 		$batch_size = empty( $max_emails ) ? $batch_size : min( $batch_size, $max_emails );
 		$batch      = array_unique( $collection->get_batched_newsletter_recipients( $options, $campaign, $batch_size, $offset ) );
-		$batch      = apply_filters( 'noptin_' . $collection->type . '_newsletter_recipients', $batch, $campaign );
+		$batch      = apply_filters( 'noptin_' . $collection->type . '_newsletter_recipients', $batch, $campaign, $options, $batch_size, $offset );
 
 		// Cache the batch for processing.
 		if ( ! empty( $batch ) ) {
