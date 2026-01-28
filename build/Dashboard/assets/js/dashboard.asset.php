feb118e8e80e03423d036d3ef8bdd97fa98258dd
picocodes <picocodes@gmail.com>
2026-01-28 20:16:56 +0300

    4.1.5 release changes

--- build/Dashboard/assets/js/dashboard.asset.php
+++ build/Dashboard/assets/js/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '3cf8d904c0431a10a220');
+<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '7c83930560ed6dacbcd6');
