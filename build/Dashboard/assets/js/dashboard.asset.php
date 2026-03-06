b39fb72e14a2f25dae956d1899cc0bbd42360949
picocodes <picocodes@gmail.com>
2026-03-06 12:09:04 +0300

    show activity overtime when viewing a single email activity

--- build/Dashboard/assets/js/dashboard.asset.php
+++ build/Dashboard/assets/js/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '88c577a10ce5158ffa86');
+<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a08b8b1cdca4e5cfd84a');
