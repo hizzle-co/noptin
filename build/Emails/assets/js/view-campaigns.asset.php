0787745242180acbf109c03463db33a0b064d6d1
picocodes <picocodes@gmail.com>
2026-07-05 10:18:06 +0300

    build

--- build/Emails/assets/js/view-campaigns.asset.php
+++ build/Emails/assets/js/view-campaigns.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '00ffd5b7b894f7b38843');
+<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e4b202493675a67b482b');
