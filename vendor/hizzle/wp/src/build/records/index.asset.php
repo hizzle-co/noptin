9ecaecf3a03c6750a7de38ff7438c1ebc1a8a8f1
picocodes <picocodes@gmail.com>
2026-08-06 08:35:20 +0300

    composer update

--- vendor/hizzle/wp/src/build/records/index.asset.php
+++ vendor/hizzle/wp/src/build/records/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '41da5139625b4b8b6925');
+<?php return array('dependencies' => array('hizzlewp-components', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dfd421f1ba5481b85562');
