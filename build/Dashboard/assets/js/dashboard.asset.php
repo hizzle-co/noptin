ec9790396f2a1b00f9c75d7c2f84c4bf87979660
picocodes <picocodes@gmail.com>
2026-09-20 11:02:25 +0300

    move from recharts

--- build/Dashboard/assets/js/dashboard.asset.php
+++ build/Dashboard/assets/js/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '52639d11d82bed454ef5');
+<?php return array('dependencies' => array('hizzlewp-components', 'hizzlewp-interface', 'hizzlewp-records', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '5a4b06b0051f46d64a3c');
